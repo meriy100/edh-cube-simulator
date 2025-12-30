8f9b73f8744ef759913d13c0a58926d8f75f9243
meriy100 <kouta@meriy100.com>
2025-12-30 18:30:24 +0900

    remove prisma の残骸

--- src/app/api/cards/route.ts
+++ src/app/api/cards/route.ts
@@ -1,6 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
-import { Prisma } from "@prisma/client";
 
 export async function GET(req: NextRequest) {
   try {
