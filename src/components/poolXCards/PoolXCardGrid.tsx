980f6c5cc11382fe9ae4140acfe5b329d33859dd
meriy100 <kouta@meriy100.com>
2025-12-31 00:59:01 +0900

    fix(admin/pools): グリッドサイズを調整

--- src/components/poolXCards/PoolXCardGrid.tsx
+++ src/components/poolXCards/PoolXCardGrid.tsx
@@ -19,7 +19,7 @@ const PoolXCardGrid = ({ poolXCards }: Props) => {
   }
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
       {poolXCards.map((card) => (
         <div
           key={card.name}
