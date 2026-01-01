0bc5f9c959a8f0331f2856475756ec65a3df3a8c
meriy100 <kouta@meriy100.com>
2026-01-01 20:47:32 +0900

    feat: pool list item to link

--- src/components/pools/PoolList.tsx
+++ src/components/pools/PoolList.tsx
@@ -1,5 +1,6 @@
 import ListItem from "@/components/ui/ListItem.client";
 import { Pool } from "@/domain/entity/pool";
+import Link from "next/link";
 
 interface Props {
   pools: Pool[];
@@ -9,7 +10,9 @@ const PoolsList = ({ pools }: Props) => {
   return (
     <div className="flex flex-col gap-2">
       {pools.map((p) => (
-        <ListItem key={p.id} title={p.version} subtitle={p.createdAt.toLocaleDateString()} />
+        <Link key={p.id} href={`/admin/pools/${p.id}`}>
+          <ListItem key={p.id} title={p.version} subtitle={p.createdAt.toLocaleDateString()} />
+        </Link>
       ))}
     </div>
   );
