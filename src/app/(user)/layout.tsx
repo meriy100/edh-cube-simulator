928ce842f4a4fc0245093b580cba7a09ac69b215
meriy100 <kouta@meriy100.com>
2026-01-11 22:35:09 +0900

    fix: user page centered

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,7 +1,14 @@
 import { PropsWithChildren } from "react";
 
 const UserLayout = ({ children }: PropsWithChildren) => {
-  return <div className="min-h-screen bg-gray-50 dark:bg-gray-900 p-6 sm:p-10">{children}</div>;
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {children}
+      </div>
+    </div>
+  );
+
 };
 
 export default UserLayout;
